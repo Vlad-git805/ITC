eee454828eb0ee639a03297e919cce8d931a00e2
Василь Колотюк <vasakolotuk02@gmail.com>
2021-03-30 19:53:53 +0300

    Creating LibraryDbContext

---
+++ MusicPlayer.DAL/EF/LibraryDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MusicPlayer.DAL.EF
+{
+    public class LibraryDbContext : DbContext
+    {
+       
+        public LibraryDbContext()
+            : base("name=LibraryDbContext")
+        {
+        }
+
+       
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+ 
+}
