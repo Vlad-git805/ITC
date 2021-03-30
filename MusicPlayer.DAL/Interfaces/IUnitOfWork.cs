5981e6e98ffa9118cefb1f64a4e4b9b69a159a59
Василь Колотюк <vasakolotuk02@gmail.com>
2021-03-30 20:13:06 +0300

    created IUnitOfWork

---
+++ MusicPlayer.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using MusicPlayer.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Album> AlbumRepos { get; }
+        IRepository<Artist> ArtistRepos { get; }
+        IRepository<Playlist> PlaylistRepos { get; }
+        IRepository<Track> TrackRepos { get; }
+        IRepository<User> UserRepos { get; }
+        IRepository<Сategory> СategoryRepos { get; }
+        void Save();
+    }
+}
