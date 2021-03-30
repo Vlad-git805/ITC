2192b2a83b07410b4ef5640826686da661609789
Vlad805 <zhomyruk_ak18@nuwm.edu.ua>
2021-03-30 19:55:00 +0300

    Finish Create MVVM elements

--- MusicPlayer.UI/Commands/DelegateCommand.cs
+++ MusicPlayer.UI/Commands/DelegateCommand.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace MusicPlayer.UI.Commands
 {
-    class DelegateCommand
+    internal sealed class DelegateCommand : Command
     {
+        private static readonly Func<bool> defaultCanExecuteMethod = () => true;
+
+        private readonly Func<bool> canExecuteMethod;
+        private readonly Action executeMethod;
+
+        public DelegateCommand(Action executeMethod) :
+            this(executeMethod, defaultCanExecuteMethod)
+        {
+        }
+
+        public DelegateCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            this.canExecuteMethod = canExecuteMethod;
+            this.executeMethod = executeMethod;
+        }
+
+        protected override bool CanExecute()
+        {
+            return canExecuteMethod();
+        }
+
+        protected override void Execute()
+        {
+            executeMethod();
+        }
     }
 }
